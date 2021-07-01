b6fba8b4197bfc4f2f43c38d4dadc9edd4fc0d05
Dzmitry <dzmitrymyshko@gmail.com>
2021-07-01 23:14:56 +0300

    script number 2

---
+++ home_work_1_20_scr2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+cd /d/QA Engineering/Ksendzov/Scripts 
+
+mkdir d4 d5 d6
+
+cd d4 
+
+touch d1.txt d2.txt d3.txt d4.json d5.json 
+
+mkdir d7 d8 d9 
+
+ls
+
+mv d1.txt d7/d1.txt
+
+mv d2.txt d8/d2.txt
+
+
